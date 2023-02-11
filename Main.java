07d000dec3e27a6c444ef2e4ea1bf8612b87d7b2
cqyao <116619110+cqyao@users.noreply.github.com>
2023-02-11 10:51:21 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,32 +1,34 @@
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        Queue myQueue = new Queue(60);
-        Random rand = new Random();
-        for (int k=0; k<=15; k++) {                         // 15 min loop
-            for (int i = 0; i <= 60; i++) {                   // Create a 60 step loop
-                int result = rand.nextInt(61 - 1);   // Get random number from 1-60
-                System.out.println(result);
-                if (result % 2 == 0) {                      // If result is even number, enqueue as an e-mail
-                    myQueue.enqueue(result);
-                }
-            }
+    static Queue receiveQueue = new Queue(60);
 
-            for (int j = 0; j <= 30; j++) {
-                int result = rand.nextInt(5 - 1); // Get a 25% chance of not dequeueing.
-                if (result == 1 | result == 2 | result == 3) { // If 75% chance hit, dequeue front.
-                    myQueue.dequeue();
-                } else {                                  // If 25% chance hit, enqueue front and then dequeue front.
-                    myQueue.enqueue(myQueue.front());
-                    myQueue.dequeue();
-                }
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        // Queue emails at avg of 30 per min. Using rng of about 50% out of 60 loops
+        for (int i = 0; i <= 60; i++) {                         // Loop running from 0 to 60 times
+            int result = rand.nextInt(61 - 1);           // Random number from 1 to 60
+            if (result % 2 == 0) {                              // If random number is even, enqueue.
+                receiveQueue.enqueue(result);
             }
         }
-        System.out.println("E-mails in queue: ");
-        for(int i = 0; i<=myQueue.getSize();i++) {
-            System.out.println(myQueue.queue[i]);
+        System.out.println("before: ");
+        receiveQueue.printQueue();
+
+
+        for (int i = 0; i <= receiveQueue.getSize(); i++) {     // Loop running from 0 until counter <= Q size.
+            int result = rand.nextInt(5 - 1);            // Random number from 1 to 4
+            if (result == 1 || result == 2 || result == 3) {    // If random number equals to 1, 2, or 3...
+                receiveQueue.dequeue();                         // ... Then dequeue the front.
+            }else{                                              // If not,
+                int reQueue = receiveQueue.getFront();          // Temp assign front element to a variable
+                receiveQueue.enqueue(reQueue);                  // Enqueue the front element
+                receiveQueue.dequeue();                         // Then dequeue the front element
+            }
         }
+        System.out.println("after: ");
+        receiveQueue.printQueue();
     }
 }
 
