a7fd4287196098dfb5eb5c82dd2237add35dc5b1
cqyao <116619110+cqyao@users.noreply.github.com>
2023-02-15 15:35:12 +0800

    Most recent version as of 15 Feb 2023

--- Main.java
+++ Main.java
@@ -1,34 +1,69 @@
 import java.util.Random;
-
 public class Main {
-    static Queue receiveQueue = new Queue(60);
 
+    static int totalProcessed;
+    static int totalSent;
+    static int totalLeft;
+    static int totalReQ;
+    static int[] attempts = new int[16];
     public static void main(String[] args) {
-
         Random rand = new Random();
-        // Queue emails at avg of 30 per min. Using rng of about 50% out of 60 loops
-        for (int i = 0; i <= 60; i++) {                         // Loop running from 0 to 60 times
-            int result = rand.nextInt(61 - 1);           // Random number from 1 to 60
-            if (result % 2 == 0) {                              // If random number is even, enqueue.
-                receiveQueue.enqueue(result);
-            }
-        }
-        System.out.println("before: ");
-        receiveQueue.printQueue();
+        Queue myQueue = new Queue(1000);
 
-
-        for (int i = 0; i <= receiveQueue.getSize(); i++) {     // Loop running from 0 until counter <= Q size.
-            int result = rand.nextInt(5 - 1);            // Random number from 1 to 4
-            if (result == 1 || result == 2 || result == 3) {    // If random number equals to 1, 2, or 3...
-                receiveQueue.dequeue();                         // ... Then dequeue the front.
-            }else{                                              // If not,
-                int reQueue = receiveQueue.getFront();          // Temp assign front element to a variable
-                receiveQueue.enqueue(reQueue);                  // Enqueue the front element
-                receiveQueue.dequeue();                         // Then dequeue the front element
+// 1. Simulate the arrival of eMails
+        // Randomly generate a number between 0 and 60.
+        for (int k = 1; k <= 15; k++) {
+            int currentSent = 0;
+            int numOfAdd = rand.nextInt(61);
+            // System.out.println("Generating " + numOfAdd + " emails.");
+            totalProcessed += numOfAdd;
+            // Push x number of email into the queue Q
+            for (int i = 0; i < numOfAdd; i++) {
+                myQueue.enqueue(i);
             }
+
+//            System.out.println("Arrival Q: ");
+//            myQueue.printQ();
+
+            // 2. Simulate the processing of eMails
+            // Generate and mark 25% of eMails that fail to send.
+            final int numOfDQ = (int) (myQueue.size() * 0.25);
+            final int qSize = myQueue.size(); // This is the Q size before any deQ operations happen.
+            for (int i = 0; i < qSize; i++) {
+                int randIndex = rand.nextInt(5);
+                if (randIndex > 3) {
+                    int temp = myQueue.front();
+                    myQueue.enqueue(temp + 100);
+                    myQueue.dequeue();
+                    totalReQ++;
+                } else {
+                    myQueue.dequeue();
+                    totalSent++;
+                    currentSent++;
+                }
+            }
+            totalLeft += myQueue.size();
+            attempts[k] = (qSize - currentSent);
         }
-        System.out.println("after: ");
-        receiveQueue.printQueue();
+            System.out.printf("Total number of messages processed: %28d " +
+                    "\nAverage number of messages sent per minute: %20d\n" +
+                    "Average number of messages in queue per minute: %16d\n" +
+                    "Average number of times messages had to be requeued: %11d\n" +
+                    "Number of messages sent on 1st attempt: %16d\n" +
+                    "Number of messages sent on 2nd attempt: %16d\n" +
+                    "Number of messages sent on 3rd attempt: %16d\n" +
+                    "Number of messages sent on 4th attempt: %16d\n" +
+                    "Number of messages sent on 5th attempt: %16d\n" +
+                    "Number of messages sent on 6th attempt: %16d\n" +
+                    "Number of messages sent on 7th attempt: %16d\n" +
+                    "Number of messages sent on 8th attempt: %16d\n" +
+                    "Number of messages sent on 9th attempt: %16d\n" +
+                    "Number of messages sent on 10th attempt: %15d\n" +
+                    "Number of messages sent on 11th attempt: %15d\n" +
+                    "Number of messages sent on 12th attempt: %15d\n" +
+                    "Number of messages sent on 13th attempt: %15d\n" +
+                    "Number of messages sent on 14th attempt: %15d\n" +
+                    "Number of messages sent on 15th attempt: %15d\n", totalProcessed, (totalSent/15), (totalLeft/15), (totalReQ/15), attempts[1], attempts[2], attempts[3], attempts[4], attempts[5], attempts[6], attempts[7], attempts[8], attempts[9], attempts[10], attempts[11], attempts[12], attempts[13], attempts[14], attempts[15]);
+
     }
 }
-
--- Queue.java
+++ Queue.java
@@ -1,58 +1,56 @@
 public class Queue {
-    int[] queue;
-    int rear;
-    int front;
-    int size; // total number of elements in queue
+    private int maxSize; // Holds the max size of the queue.
+    int[] qArray; // The array to store emails in the queue
+    private int front; // Front pointer of the queue (first element)
+    private int rear; // Rear pointer of the queue (last element)
+    private int nItems;
 
-    public Queue(int sizeOfArray) {
-        queue = new int[sizeOfArray];
-        rear = 0;
+    // Constructor
+    public Queue(int qSize) {
+        maxSize = qSize;
+        qArray = new int[maxSize];
         front = 0;
-        size = 0;
+        rear = 0;
+        nItems = 0;
     }
 
-    public boolean isEmpty() { // Checks if the queue is empty and returns true or false.
-        return size==0;
+    // Queue functions
+    public void enqueue(int item) {
+        if (isFull()) {
+            System.out.println("Queue is full ");
+        }
+        qArray[rear] = item;
+        // rear = (rear+1) % qArray.length;
+        rear++;
+        nItems++;
+    }
+
+    public void dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty ");
+        }
+        front++;
+        nItems--;
+    }
+
+    public int front() {
+        return qArray[front];
+    }
+
+    public boolean isEmpty() {
+        return (nItems == 0);
     }
 
     public boolean isFull() {
-        return size==60;
+        return (nItems == maxSize);
     }
 
-    public void enqueue(int element) { //Method for queueing elements into Queue
-        if(!isFull()) {
-            queue[rear] = element;
-            rear = (rear + 1) % 60;
-            size++;
-        }
-        else {
-            System.out.println("Queue is full");
-        }
+    public int size() {
+        return nItems;
     }
-
-    public void dequeue() { // Method for dequeueing front element
-        if(!isEmpty()) {
-            int data = queue[front];
-            front = (front + 1) % 60;
-            size--;
+    public void printQ() {
+        for (int i = 0; i<nItems;i++) {
+            System.out.println(qArray[i+front]);
         }
-        else {
-            System.out.println("Queue is empty");
-        }
-    }
-
-    public void printQueue() {
-        for(int i=0; i<size; i++)
-        {
-            System.out.println(queue[(front+i)%getSize()]);
-        }
-    }
-
-    public int getFront() { // Returns element at front
-        return queue[front];
-    }
-
-    public int getSize() {  // Returns size of queue
-        return size;
     }
 }
